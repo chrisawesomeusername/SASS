646dd2c8832f3853da32d9efc7a077be8ff10e5a
chrisawesomeusername <chrisawesomeusername@gmail.com>
2019-08-14 15:31:10 +1200

    VERY COOL AND FUN

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+$(document).ready(function(){
+
+$('.fas').click(function(){
+	$('nav').toggleClass('active');
+})
+
+// $('#close').click(function(){
+// 	$('nav').hide();
+// })
+
+
+
+});
