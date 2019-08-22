ca48e24d73b22608836128cf2701b6a650e458b6
chrisawesomeusername <chrisawesomeusername@gmail.com>
2019-08-22 15:18:05 +1200

    afdaj

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 console.log('hello');
 
+$('.morePara').hide();
+$('.hideBtn').hide();
+
+
+
 $(document).ready(function(){
 
-$('.fas').click(function(){
-	$('nav').toggleClass('active');
+	$('.fas').click(function(){
+		$('nav').toggleClass('active');
 })
 
 // $('#close').click(function(){
@@ -12,4 +17,27 @@ $('.fas').click(function(){
 
 
 
-});
+//  $('.showBtn').click(function(){
+// 	 $('.morePara').show();
+// 	 $('hideBtn').show();
+// 	 $(this).hide();
+//
+//
+// })
+//
+// $('.hideBtn').click(function(){
+// 	$('.morePara').hide();
+// 	$(this).hide();
+// 	$('showBtn').show();
+// })
+
+$('.showBtn').click(function(){
+	$('.morePara').fadeToggle();
+})
+
+$('.variables').click(function() {
+	$('html,body').animate({
+		scrollTop: $('#list1').offset().top }, 3000);
+	})
+
+})
